f4884b79c7703d1c83cc2d3735b50af2c9553120
DanielRevenga <daniel.revenga.garcia@gmail.com>
2022-02-03 11:51:07 +0100

    testing_apiResponse

--- src/features/contacts/contactsSlice.tsx
+++ src/features/contacts/contactsSlice.tsx
@@ -25,7 +25,7 @@ export const getContacts: any = createAsyncThunk(
             'Content-Type': 'application/json'  
         }
 
-        return await axios.get(`http://(process.env.AZURE_HOST_NAME:5000/dashboard/contacts`, headers);
+        return await axios.get(`https://mirandaback2.azurewebsites.net/dashboard/contacts`, headers);
         // return await axios.get("http://localhost:5000/dashboard/contacts", headers);
     }
 )
